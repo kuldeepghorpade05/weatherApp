e3ac6bcee7ea6a97b012e4adbeb290a756a97363
kuldeep ghorpade <kuldeepghorpade05@gmail.com>
2024-09-19 23:33:51 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// alert('okay');
+
+// getting button element through getelementbyid
+const button = document.getElementById("search-button");
+const input = document.getElementById("city-input");
+
+
+const cityname = document.getElementById('city-name');
+const citytime = document.getElementById('city-time');
+const citytemp = document.getElementById('city-temp');
+const cityhumidity = document.getElementById('city-humidity');
+
+const weatherimg = document.getElementById('weather-img');
+
+
+
+async function getdata(cityname){
+    const promise = await fetch(
+        `http://api.weatherapi.com/v1/current.json?key=8fe6dbbe334d4ebb84773811241909&q=${cityname}&aqi=yes`
+    );
+    return await promise.json();
+}
+
+
+
+button.addEventListener('click', async () => {
+    const value = input.value;
+    const result = await getdata(value);
+    // console.log(result);
+    cityname.innerText =`Location : ${result.location.name}, ${result.location.region} - ${result.location.country}`;
+    citytime.innerText = `LocalTime : ${result.location.localtime}`;
+    citytemp.innerText = `Temperature : ${result.current.temp_c}`;
+    cityhumidity.innerText = `Humidity : ${result.current.humidity}`;
+
+     // Update the weather image
+     weatherimg.src = `https:${result.current.condition.icon}`; // Full URL for the weather icon
+     weatherimg.alt = result.current.condition.text; // Set alt text based on weather condition
+});
+
+
+// 8fe6dbbe334d4ebb84773811241909
+
+// http://api.weatherapi.com/v1/current.json?key=8fe6dbbe334d4ebb84773811241909&q=London&aqi=yes
+
